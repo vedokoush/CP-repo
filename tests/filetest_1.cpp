8aa6ebcd4f65fb390ad8d54d62916189f55c5f12
shouko.-. <144153287+skotish06@users.noreply.github.com>
2025-10-31 18:02:27 +0700

    code

--- tests/filetest_1.cpp
+++ tests/filetest_1.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
-#define int long long
-#define task ""
 using namespace std;
-const int N = 1e6 + 9;
-int n, k, h[N], f[N];
-void solve(){
-    cin >> n >> k;
-    for (int i = 1; i <= n; ++i) {
-        cin >> h[i];
-    }
-    f[1] = 0;
-    for (int i = 2; i <= n; ++i) {
-        f[i] = 1e18;
-        for (int j = i - k; j <= i - 1; ++j) {
-            if (j >= 1) 
-                f[i] = min(f[i], f[j] + abs(h[j] - h[i]));
-        }
-    }
-    cout << f[n];
+#define int long long
+#define task "SPNUM"
+#define all(v) v.begin(), v.end()
+#define ms(d,x) memset(d, x, sizeof(d))
+
+bool prime[1000006];
+
+void sieve(int n){
+    ms(prime, true);
+    prime[0] = prime[1] = false;
+    for(int i=2; i*i<=n; i++)
+        if(prime[i])
+            for(int j=i*i; j<=n; j+=i)
+                prime[j] = false;
 }
-signed main(){
-    {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        if(fopen(task ".inp", "r"))
-        {
-            freopen(task ".inp", "r", stdin);
-            freopen(task ".out", "w", stdout);
-        }
-    }
-    solve();
+
+void logic(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, k; cin >> n >> k;
+    sieve(n);
+    
+    int ans = 0;
+    for(int i=2; i<=n; i++)
+        if(prime[i] && __builtin_popcount(i) == k)
+            ans++;
+    
+    cout << ans;
+}
+
+int32_t main(){
+    #ifdef LOCAL
+    freopen(task".INP", "r", stdin);
+    freopen(task".OUT", "w", stdout);
+    #endif
+    logic();
 }
